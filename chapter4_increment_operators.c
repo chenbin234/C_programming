c5204b7f0c7d5efaf1c64835724c0700e6726162
chenbingcheng <bingcheng.chen@outlook.com>
2023-08-12 14:39:49 +0200

    update

---
+++ chapter4_increment_operators.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i,j;
+
+    i = 1;
+    printf("i is %d\n", ++i);
+    printf("i is %d\n", i);
+
+    j = 1;
+    printf("j is %d\n", j++);
+    printf("j is %d\n", j);
+
+
+}
